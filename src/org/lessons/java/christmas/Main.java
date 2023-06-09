5e860eefa0f8d144d16cd3d64412ec6d8079d34f
Daniel <angelicodaniel93@gmail.com>
2023-06-09 14:20:19 +0200

    gift list

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,7 +1,38 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> giftList = new ArrayList<>();
+        System.out.println("La lista è vuota? " + giftList.isEmpty());
 
+        boolean stop = false;
+
+        do {
+            System.out.print("Inserisci il nome del regalo da aggiungere alla lista (oppure 'stop' per fermarti): ");
+            String gift = scanner.nextLine();
+
+            switch (gift) {
+                case "stop":
+                    stop = true;
+                    break;
+                default:
+                    giftList.add(gift);
+                    System.out.println("Lunghezza della lista: " + giftList.size());
+                    break;
+            }
+        } while (!stop);
+
+        Collections.sort(giftList);
+
+        System.out.println("Gift list:");
+        for (String item : giftList) {
+            System.out.println(item);
+        }
+        scanner.close();
     }
 }
